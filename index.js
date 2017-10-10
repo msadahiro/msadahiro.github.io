a2167455e0067530d61ac1856884af31342222cb
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-10-10 14:57:41 -0700

    updated portfolio, added work experience

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+  $("a").on('click', function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function () {
         window.location.hash = hash;
       });
     }
   });
-  $("#changeBackground").click(function() {
-        $("body").toggleClass("changeBackground"); 
-    });
+  $("#changeBackground").click(function () {
+    $("body").toggleClass("changeBackground");
+    $("#adjustIconColor").toggleClass("adjustIconColor")
+  });
+  $('#meetingPeopleDiv').hover(function () {
+    $(this).find('#meetingOthersLabel').removeClass("hiddenLabel");
+    $(this).find('#meetingOthersLabel').addClass("projectLabel");
+  }, function () {
+    $(this).find('#meetingOthersLabel').addClass("hiddenLabel");
+    $(this).find('#meetingOthersLabel').removeClass("projectLabel");
+  });
+  $('#txGeneticsDiv').hover(function () {
+    $(this).find('#txGeneticsLabel').removeClass("hiddenLabel");
+    $(this).find('#txGeneticsLabel').addClass("projectLabel");
+  }, function () {
+    $(this).find('#txGeneticsLabel').addClass("hiddenLabel");
+    $(this).find('#txGeneticsLabel').removeClass("projectLabel");
+  });
 });
+
+const modalNav = document.getElementById("myNav")
+const openMenu = (x) => {
+  x.classList.toggle("change")
+}
+
+
 
