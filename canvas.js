ce8ad9d2fb99d7228d07c2c20f510d036bb784cb
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-12-17 21:50:37 -0800

    reformated layout

--- canvas.js
+++ canvas.js
@@ -7,8 +7,8 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 var mouse = {
-    x:undefined,
-    y:undefined
+    x: undefined,
+    y: undefined
 }
 var maxRadius = 400;
 
@@ -20,18 +20,18 @@ var colorArray = [
     '#F2EFDF',
 ];
 
-window.addEventListener('mousemove', function(){
+window.addEventListener('mousemove', function () {
     mouse.x = event.x;
     mouse.y = event.y;
 });
 
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     init();
 })
 
-function Circle(x, y, dx, dy, radius){
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
     this.dx = dx;
@@ -39,29 +39,29 @@ function Circle(x, y, dx, dy, radius){
     this.radius = radius;
     this.minRadius = radius;
     this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
-    this.draw = function(){
+    this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
     }
-    this.update = function(){
-        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
         }
-        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
             this.dy = -this.dy;
         }
         this.x += this.dx;
         this.y += this.dy;
 
         // interactivity
-        if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50){
-            if(this.radius < maxRadius){
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.radius < maxRadius) {
                 this.radius += 1;
             }
         }
-        else if(this.radius > this.minRadius){
+        else if (this.radius > this.minRadius) {
             this.radius -= 1;
         }
         this.draw();
@@ -71,25 +71,25 @@ function Circle(x, y, dx, dy, radius){
 // holds all my circles
 var circleArray = [];
 
-function init(){
+function init() {
 
     circleArray = [];
 
     // creating the cirlcle
-    for(var i = 0; i < 50; i++){
+    for (var i = 0; i < 25; i++) {
         var radius = Math.random() * 3 + 100;
         var x = Math.random() * (innerWidth - radius * 2) + radius;
         var y = Math.random() * (innerHeight - radius * 2) + radius;
         var dx = (Math.random() - 0.5);
         var dy = (Math.random() - 0.5);
-        circleArray.push(new Circle(x,y,dx,dy,radius));
+        circleArray.push(new Circle(x, y, dx, dy, radius));
     }
 }
-function animate (){
+function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0,0,innerWidth,innerHeight);
+    c.clearRect(0, 0, innerWidth, innerHeight);
 
-    for(var i = 0; i < circleArray.length; i++){
+    for (var i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
     }
 }
